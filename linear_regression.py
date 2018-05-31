b50dbf10b8598c5752bcce7cb3e886474575804e
Max Quinn <maxquinn@maxs-mbp.usny.ibm.com>
2018-05-31 11:06:00 -0400

    normalization

---
+++ linear_regression.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#Normalizes matrix values to values between -1 and 1
+def normalize(data):
+	#data: training examples x features
+	for feature in data.T:
+		mean = np.mean(feature)
+		range = np.max(feature) - np.min(feature)
+		print(mean, range)
+		feature -= mean
+		feature /= range
+	return data
+
+#arr = np.array([[1000.0, 32.0],[2000.0,4.0], [10000.0, 37.0]])
+#print(normalize(arr))
