d9dad311202f050fe91469eb1cb9af2df716f25b
Max Quinn <maxquinn@maxs-mbp.usny.ibm.com>
2018-05-31 11:32:13 -0400

    prediction

--- linear_regression.py
+++ linear_regression.py
@@ -6,10 +6,15 @@ def normalize(data):
 	for feature in data.T:
 		mean = np.mean(feature)
 		range = np.max(feature) - np.min(feature)
-		print(mean, range)
+		#print(mean, range)
 		feature -= mean
 		feature /= range
 	return data
 
 #arr = np.array([[1000.0, 32.0],[2000.0,4.0], [10000.0, 37.0]])
 #print(normalize(arr))
+
+def predict(data, params):
+	#data: data predictions x features
+	#params: features x 1
+	return np.dot(data, params)  
